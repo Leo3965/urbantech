d2787b3b920da00968ed5b989eda0c2f2d914d63
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-10-29 21:02:48 -0300

    feat: create second modal

---
+++ pages/home/home.js
@@ -0,0 +1,31 @@
+// Função para mostrar o modal de cookies após 5 segundos
+setTimeout(function () {
+  document.getElementById("cookieModal").style.display = "block";
+  document.getElementById("modalBackground").style.display = "block";
+}, 5 * 1000);
+
+// Função para ocultar o modal de cookies e o fundo escurecido
+function handleCookieConsent() {
+  document.getElementById("cookieModal").style.display = "none";
+  document.getElementById("modalBackground").style.display = "none";
+}
+
+// Função para mostrar o modal de administração de cookies
+function openAdminModal() {
+  document.getElementById("cookieModal").style.display = "none"; // Esconde o primeiro modal
+  document.getElementById("adminModal").style.display = "block"; // Mostra o modal de administração
+}
+
+// Função para aceitar todos os cookies e fechar o modal
+function acceptAllCookies() {
+  document.getElementById("adminModal").style.display = "none";
+  document.getElementById("modalBackground").style.display = "none";
+  alert("Todos os cookies foram aceitos.");
+}
+
+// Função para rejeitar todos os cookies e fechar o modal
+function rejectAllCookies() {
+  document.getElementById("adminModal").style.display = "none";
+  document.getElementById("modalBackground").style.display = "none";
+  alert("Todos os cookies foram rejeitados.");
+}
